f2c9c8c02d943d4324060a281ae1be2030f216df
Brian Sadler <briansadler@macbook-pro-3.local>
2018-08-20 14:32:59 -0500

    Initial Commit

---
+++ ModelACar/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ModelACar
+//
+//  Created by Brian Sadler on 8/20/18.
+//  Copyright © 2018 Brian Sadler. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
